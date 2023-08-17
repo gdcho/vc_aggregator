6b283e0af2d6ebd1014a789196ec1a5d5c19f7e4
David Cho <gcho13@my.bcit.ca>
2023-08-17 17:18:40 -0400

    [Add] Environment dotenv

---
+++ environment_variables.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+twitter_api_key = os.getenv("TWITTER_API_KEY")
+tiktok_api_key = os.getenv("TIKTOK_API_KEY")
+youtube_api_key = os.getenv("YOUTUBE_API_KEY")
