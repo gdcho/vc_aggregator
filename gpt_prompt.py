b88a760dac725e88e06bd1dc339963c499d51c33
David Cho <gcho13@my.bcit.ca>
2023-08-17 21:18:25 -0400

    [Modify] Include pexels for stock free videos

--- gpt_prompt.py
+++ gpt_prompt.py
@@ -1,45 +1,52 @@
 import openai
-from googleapiclient.discovery import build
+import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-youtube_api_key = os.getenv("YOUTUBE_API_KEY")
+pexels_api_key = os.getenv("PEXELS_API_KEY")
 openai.api_key = api_key
-youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
 prompt = "Give me around 75 words based on an interesting fact."
 
 response = openai.Completion.create(
-    engine="text-davinci-003",  
+    engine="text-davinci-003",
     prompt=prompt,
-    max_tokens=200,  
-    stop=None 
+    max_tokens=200,
+    stop=None
 )
 
 generated_fact = response.choices[0].text.strip()
 
 print("Generated Fact:", generated_fact)
 
-prompt2 = f"Based on the generated fact, {generated_fact}, give me three subjects."
+prompt2 = f"Based on the generated fact, {generated_fact}, return the one main subject noun."
 
 response = openai.Completion.create(
-    engine="text-davinci-003",  
+    engine="text-davinci-003",
     prompt=prompt2,
-    max_tokens=30,  
-    stop=None 
+    max_tokens=30,
+    stop=None
 )
 
 generated_keywords = response.choices[0].text.strip()
 
 print("\nGenerated Keywords:", generated_keywords)
 
-# YouTube - Fetch video data
-search_query = f"{generated_keywords}" 
-request = youtube.search().list(q=search_query, part='snippet', maxResults=10)
-response = request.execute()
+# Pexels - Fetch video data
+pexels_url = f"https://api.pexels.com/videos/search?query={generated_keywords}&per_page=2"
+headers = {"Authorization": pexels_api_key}
+response = requests.get(pexels_url, headers=headers)
+pexels_data = response.json()
 
-for item in response['items']:
-    print(item['snippet']['title'])
-    print(item['snippet']['description'])
+if 'videos' in pexels_data:
+    videos = pexels_data['videos']
+    if videos:
+        video = videos[0]
+        print("Video URL:", video['url'])
+        print("Video Title:", video['user']['name'])
+    else:
+        print("No videos found.")
+else:
+    print("No videos found in the response.")
