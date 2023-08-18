30d04a1f9f8843bd05d1e8573d0f7311a7040a07
David Cho <gcho13@my.bcit.ca>
2023-08-18 17:17:45 -0400

    [Add] Implement Youtube googleapiclient to find stock video

--- gpt_prompt.py
+++ gpt_prompt.py
@@ -5,60 +5,75 @@ import os
 from gtts import gTTS
 import IPython.display as ipd
 from io import BytesIO
+from googleapiclient.discovery import build
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 pexels_api_key = os.getenv("PEXELS_API_KEY")
 openai.api_key = api_key
+youtube_api_key = os.getenv("YOUTUBE_API_KEY")
+youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-prompt = "Give me around 75 words based on an interesting fact."
 
-response = openai.Completion.create(
+def generate_fact_prompt():
+    return "Give me around 75 words based on an interesting fact."
+
+
+def generate_subject_noun_prompt(fact):
+    return f"Based on the generated fact, {fact}, return a main subject noun."
+
+
+def fetch_pexels_videos(keyword):
+    pexels_url = f"https://api.pexels.com/videos/search?query={keyword}&per_page=2"
+    headers = {"Authorization": pexels_api_key}
+    response = requests.get(pexels_url, headers=headers)
+    pexels_data = response.json()
+    return pexels_data.get('videos', [])
+
+
+fact_prompt = generate_fact_prompt()
+fact_response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt=prompt,
+    prompt=fact_prompt,
     max_tokens=200,
     stop=None
 )
+generated_fact = fact_response.choices[0].text.strip()
 
-generated_fact = response.choices[0].text.strip()
-
-print("Generated Fact:", generated_fact)
-
-prompt2 = f"Based on the generated fact, {generated_fact}, return the one main subject noun."
-
-response = openai.Completion.create(
+noun_prompt = generate_subject_noun_prompt(generated_fact)
+noun_response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt=prompt2,
+    prompt=noun_prompt,
     max_tokens=30,
     stop=None
 )
+generated_noun = noun_response.choices[0].text.strip()
 
-generated_keywords = response.choices[0].text.strip()
+# Fetch Pexels
+videos = fetch_pexels_videos(generated_noun)
 
-print("\nGenerated Keywords:", generated_keywords)
-
-# Pexels - Fetch video data
-pexels_url = f"https://api.pexels.com/videos/search?query={generated_keywords}&per_page=2"
-headers = {"Authorization": pexels_api_key}
-response = requests.get(pexels_url, headers=headers)
-pexels_data = response.json()
-
-if 'videos' in pexels_data:
-    videos = pexels_data['videos']
-    if videos:
-        video = videos[0]
-        print("Video URL:", video['url'])
-        print("Video Title:", video['user']['name'])
-    else:
-        print("No videos found.")
+print(f"\nGenerated Fact: {generated_fact}")
+print(f"\nGenerated Noun: {generated_noun}\n")
+if videos:
+    for video in videos:
+        print(f"Pexel URL: {video['url']}")
 else:
-    print("No videos found in the response.")
+    print("No videos found.")
+
+# Fetch YouTube
+search_query = generated_noun + " free stock footage"
+request = youtube.search().list(q=search_query, part='snippet', maxResults=3)
+response = request.execute()
+print("\nYouTube:")
+for videos in response['items']:
+    print(
+        f"YouTube Video Link: https://www.youtube.com/watch?v={videos['id']['videoId']}")
 
 # gTTS - Text to Speech
 tts = gTTS(generated_fact, lang='en', tld='com.au', slow=False)
 audio_bytes = BytesIO()
 tts.write_to_fp(audio_bytes)
-audio_bytes.seek(0) 
+audio_bytes.seek(0)
 
 audio_filename = "generated_fact.mp3"
 with open(audio_filename, "wb") as f:
