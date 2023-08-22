004df1317b23e5498cb4b08eeca78b62b1990566
David Cho <gcho13@my.bcit.ca>
2023-08-22 01:25:50 -0400

    [Modify] Output to mimic YouTube Shorts layout

--- aggregrate_fv1.py
+++ aggregrate_fv1.py
@@ -3,15 +3,13 @@ import requests
 from dotenv import load_dotenv
 import os
 from gtts import gTTS
-import IPython.display as ipd
 from io import BytesIO
 from googleapiclient.discovery import build
 from moviepy.editor import AudioFileClip, VideoFileClip, TextClip, CompositeVideoClip, concatenate_videoclips
-import shutil
 import urllib.request
 import ssl
-import pytube
-
+from pytube import YouTube
+from moviepy.video.io.VideoFileClip import VideoFileClip
 ssl._create_default_https_context = ssl._create_unverified_context
 
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
@@ -33,11 +31,12 @@ def generate_subject_noun_prompt(fact):
 
 
 def fetch_pexels_videos(keyword):
-    pexels_url = f"https://api.pexels.com/videos/search?query={keyword}&per_page=2"
+    pexels_url = f"https://api.pexels.com/videos/search?query={keyword}&per_page=3"
     headers = {"Authorization": pexels_api_key}
     response = requests.get(pexels_url, headers=headers, timeout=30)
     pexels_data = response.json()
     return pexels_data.get('videos', [])
+
 
 fact_prompt = generate_fact_prompt()
 fact_response = openai.Completion.create(
@@ -93,51 +92,62 @@ with open(temp_audio_filename, "wb") as f:
 tts_audio_clip = AudioFileClip(temp_audio_filename)
 os.remove(temp_audio_filename)
 
-# Build with MoviePy
+# Fetch Pexels
 pexels_videos = fetch_pexels_videos(generated_noun)
 pexels_video_clips = []
 
 for video_info in pexels_videos:
     video_files = video_info.get('video_files', [])
-    
+
     if video_files:
-        video_url = video_files[0].get('link', '')
-        
+        video_url = next(
+            (file['link'] for file in video_files if file['file_type'] == 'video/mp4'), None)
+
         if video_url:
             video_filename = video_url.split('/')[-1]
             urllib.request.urlretrieve(video_url, video_filename)
-            pexels_video_clip = VideoFileClip(video_filename)
+            pexels_video_clip = VideoFileClip(
+                video_filename).subclip(0, 20)
             pexels_video_clips.append(pexels_video_clip)
-            os.remove(video_filename)
 
-
+# Fetch YouTube
 youtube_video_clips = []
-for video in response['items']:
-    youtube_video_url = f"https://www.youtube.com/watch?v={video['id']['videoId']}"
 
-    yt = pytube.YouTube(youtube_video_url)
-    video_stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
-    video_filename = f"youtube_video_{video['id']['videoId']}.mp4"
+for videos in response['items']:
+    youtube_video_url = f"https://www.youtube.com/watch?v={videos['id']['videoId']}"
+
+    yt = YouTube(youtube_video_url)
+    video_stream = yt.streams.filter(
+        progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+    video_filename = f"youtube_video_{videos['id']['videoId']}.mp4"
     video_stream.download(output_path="", filename=video_filename)
-
-    youtube_video_clip = VideoFileClip(video_filename)
+    youtube_video_clip = VideoFileClip(
+        video_filename).subclip(0, 10)
     youtube_video_clips.append(youtube_video_clip)
 
-# MoviePy text clip
-text_clip = TextClip(generated_fact, fontsize=20, color='white').set_duration(60)
+video_width = 1080
+video_height = 1920
 
-# Stack the Pexels and YouTube
-stacked_pexels_videos = concatenate_videoclips(pexels_video_clips)
-stacked_youtube_videos = concatenate_videoclips(youtube_video_clips)
+text_clip = TextClip(generated_fact, fontsize=20,
+                     color='white').set_duration(60)
+text_clip = text_clip.set_position(('center', video_height // 2))
 
-# Combine video and text
-final_video = CompositeVideoClip([text_clip.set_position('center'), stacked_pexels_videos.set_position('top'),
-                                 stacked_youtube_videos.set_position('bottom')])
+stacked_pexels_videos = concatenate_videoclips(
+    pexels_video_clips, method="compose")
+stacked_pexels_videos = stacked_pexels_videos.set_position(
+    ('center', video_height // 2))
 
+stacked_youtube_videos = concatenate_videoclips(
+    youtube_video_clips, method="compose")
+stacked_youtube_videos = stacked_youtube_videos.set_position(
+    ('center', video_height - stacked_youtube_videos.h))
+
+final_video = CompositeVideoClip(
+    [text_clip, stacked_pexels_videos, stacked_youtube_videos], size=(video_width, video_height))
+tts_audio_clip = tts_audio_clip.volumex(0.9) 
 final_video = final_video.set_audio(tts_audio_clip)
+print("TTS Audio Duration:", tts_audio_clip.duration)
 
-output_video_path = "final_video.mp4"
-final_video.write_videofile(output_video_path, codec="libx264", audio_codec="aac")
-
-final_video.preview()
-
+output_video_path = "final_video_shorts.mp4"
+final_video.write_videofile(
+    output_video_path, codec="libx264", audio_codec="aac", threads=4)
